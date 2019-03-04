df5bef6264d3912b47b9074af840b5c119e3aa79
rich0285 <44639744+rich0285@users.noreply.github.com>
2019-03-04 11:52:18 +0100

    bestestbestBot

--- src/dk/easv/bll/bot/BestBot.java
+++ src/dk/easv/bll/bot/BestBot.java
@@ -5,6 +5,11 @@
  */
 package dk.easv.bll.bot;
 
+import dk.easv.bll.field.IField;
+import dk.easv.bll.game.IGameState;
+import dk.easv.bll.move.IMove;
+import dk.easv.bll.move.Move;
+
 /**
  *
  * @author Richart hansen
@@ -15,11 +20,35 @@ public class BestBot extends LocalPrioritisedListBot
     int i = 1;
    
     public BestBot(){
+        
          int[][] pref = {
-             {2,0},{1,1},{0,2},
-             {2,2},{2,1},{1,2},
-             {0,1},{1,0},{0,0},};
+             {1,1},{2,0},{0,2},};
+         
         super.preferredMoves = pref;
+    }
+     @Override
+    public IMove doMove(IGameState state) {
+
+        //Find macroboard to play in
+        for (int[] move : preferredMoves)
+        {
+            if(state.getField().getMacroboard()[move[0]][move[1]].equals(IField.AVAILABLE_FIELD))
+            {
+                //find move to play
+                for (int[] selectedMove : preferredMoves)
+                {
+                    int x = move[0]*3 + selectedMove[0];
+                    int y = move[1]*3 + selectedMove[1];
+                    if(state.getField().getBoard()[x][y].equals(IField.EMPTY_FIELD))
+                    {
+                        return new Move(x,y);
+                    }
+                }
+            }
+        }
+
+        //NOTE: Something failed, just take the first available move I guess!
+        return state.getField().getAvailableMoves().get(0);
     }
     
     
